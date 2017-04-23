11c845649a215e6ea41c4dea8070d50fa88ff802
Jan-Erik Schonewille <jan-erik.schonewille@imc.com>
2017-04-23 23:23:54 +0200

    Deploy on Heroku

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Jar
+
 repositories {
     mavenLocal()
     maven { setUrl("http://repo.maven.apache.org/maven2") }
@@ -5,12 +8,36 @@ repositories {
 
 plugins {
     java
+    id("com.github.johnrengelman.shadow") version "1.2.4"
 }
+
+task<Copy>("copyToLib") {
+    from(configurations.compile)
+    into("build/libs")
+}
+
+val jar: Jar by tasks
+
+jar.apply {
+    manifest.attributes.apply {
+        put("Main-Class", "com.stylismo.playground.vaadin.Main")
+    }
+}
+
 
 object versions {
     val vaadin = "8.0.2"
     val spark = "2.5.6-SNAPSHOT"
 }
+
+tasks {
+    "stage" {
+        dependsOn("build", "shadowJar")
+
+        doLast { println("Built spark-vaadin-all.jar") }
+    }
+}
+
 
 dependencies {
     compile("com.vaadin:vaadin-server:${versions.vaadin}")
--- src/main/java/com/stylismo/playground/vaadin/Main.java
+++ src/main/java/com/stylismo/playground/vaadin/Main.java
@@ -6,13 +6,19 @@ import static spark.Spark.*;
 
 
 public class Main {
+    public static final String DEFAULT_PORT = "8080";
+    public static final String PORT = "server.port";
 
     public static void main(String[] args) throws Exception {
-        port(8080);
+        port(getHerokuAssignedPort());
 
         servlet("/app/*", SparkVaadinServlet.class);
         servlet("/VAADIN/*", SparkVaadinServlet.class);
 
         init();
+    }
+
+    static int getHerokuAssignedPort() {
+        return Integer.parseInt(System.getProperty(PORT, DEFAULT_PORT));
     }
 }
